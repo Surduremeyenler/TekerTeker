991d240ff0a13c7d11799f98c104bdef514f15b7
alper50 <alperyn50@gmail.comm>
2022-02-11 23:10:48 +0300

    fix: bottomnavbar changed

--- lib/infrastructure/consts.dart
+++ lib/infrastructure/consts.dart
@@ -1,3 +1,3 @@
-const String bottomNavBar1 = 'Home';
+const String bottomNavBar1 = 'Map';
 const String bottomNavBar2 = 'Credits';
 const String bottomNavBar3 = 'Profile';
--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -14,19 +14,18 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final List<Widget> _screens = [
-    MapPage(),
     CreditPage(),
+    MapPage(),
     ProfilePage(),
-   
   ];
    final items = <BottomNavigationBarItem>[
       BottomNavigationBarItem(
-        label: bottomNavBar1,
-        icon: Icon(Icons.map),
-      ),
-      BottomNavigationBarItem(
         label: bottomNavBar2,
         icon: Icon(Icons.cabin),
+      ),
+      BottomNavigationBarItem(
+        label: bottomNavBar1,
+        icon: Icon(Icons.map),
       ),
       BottomNavigationBarItem(
         label: bottomNavBar3,

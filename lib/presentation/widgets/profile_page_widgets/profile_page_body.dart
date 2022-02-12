6e1b2863a952eef4c97d95eb80bd787ec4f0bd0a
efesahin35 <efe.mate@gmail.com>
2022-02-13 02:57:57 +0300

    fix: profile page is freezing a seconds

--- lib/presentation/widgets/profile_page_widgets/profile_page_body.dart
+++ lib/presentation/widgets/profile_page_widgets/profile_page_body.dart
@@ -43,7 +43,7 @@ class ProfilePageBody extends StatelessWidget {
               );
             }
           }
-          return Container();
+          return Center(child: CircularProgressIndicator());
         });
   }
 }

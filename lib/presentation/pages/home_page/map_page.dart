c1ba20bb19df34d0a0b8191f40d0563062d128f4
alper50 <alperyn50@gmail.comm>
2022-02-12 19:44:29 +0300

    added qr scanner

--- lib/presentation/pages/home_page/map_page.dart
+++ lib/presentation/pages/home_page/map_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:teker_teker/application/map/map_cubit.dart';
 import 'package:teker_teker/presentation/constants/constants.dart';
+import 'package:teker_teker/presentation/pages/qr_scanner_page.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({Key? key}) : super(key: key);
@@ -35,10 +36,34 @@ class _MapPageState extends State<MapPage> {
                 },
               ),
               Positioned(
-                child: InkWell(
-                  onTap: (){},
-                  child: SizedBox(
-                    child: Text('Sürüşe Başla'),
+                width: MediaQuery.of(context).size.width,
+                bottom: 40,
+                child: Center(
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const QrScannerPage()),
+                      );
+                    },
+                    child: Container(
+                      width: MediaQuery.of(context).size.width / 1.5,
+                      height: 45,
+                      decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Sürüşe Başla',
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white70,
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
                 ),
               ),
---
+++ lib/presentation/pages/qr_scanner_page.dart
@@ -0,0 +1,70 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+class QrScannerPage extends StatefulWidget {
+  const QrScannerPage({ Key? key }) : super(key: key);
+
+  @override
+  State<QrScannerPage> createState() => _QrScannerPageState();
+}
+
+class _QrScannerPageState extends State<QrScannerPage> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  Barcode? result;
+  QRViewController? controller;
+
+ 
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller!.pauseCamera();
+    } else if (Platform.isIOS) {
+      controller!.resumeCamera();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 5,
+            child: QRView(
+              key: qrKey,
+              onQRViewCreated: _onQRViewCreated,
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: (result != null)
+                  ? Text(
+                      'Barcode Type: ${describeEnum(result!.format)}   Data: ${result!.code}')
+                  : Text('Scan a code'),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+}
